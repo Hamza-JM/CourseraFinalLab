cf1f178ebefe3a1db762a24c6c13fad283a2a5b3
Hamza Jomaa <hamza.jomaa@supcom.tn>
2024-01-31 23:34:12 +0100

    Your commit message

---
+++ src/components/Card.jsx
@@ -0,0 +1,34 @@
+import { Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+
+const Card = ({ title, description, imageSrc }) => {
+  return (
+    <VStack
+      color="black"
+      backgroundColor="white"
+      cursor="pointer"
+      borderRadius="x1"
+    >
+      <Image src={imageSrc} borderRadius="x1"></Image>
+      <VStack spacing={4} p={4} alignItems="flex-start">
+        <HStack justifyContent="space-between" alignItems="center">
+          <Heading as="h3" size="md" noOfLines={1}>
+            {title}
+          </Heading>
+        </HStack>
+
+        <Text fontSize="1g" color="#64748b">
+          {description}
+        </Text>
+        <HStack>
+          <p>See more</p>
+          <FontAwesomeIcon icon={faArrowRight} size="1x" />
+        </HStack>
+      </VStack>
+    </VStack>
+  );
+};
+
+export default Card;
